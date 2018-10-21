24691e9d93941c06ee96fa2f95b3d988d2889a39
wangrruir <wangrruir@gmail.com>
2018-10-21 15:40:14 +0800

    readme.md

---
+++ util.py
@@ -0,0 +1,4 @@
+trainDataPath='/home/wangrui/data/20news-18828/train/'
+valDataPath='/home/wangrui/data/20news-18828/val/'
+dataHandledPath='/home/wangrui/data/20news-18828/handle'
+tfidfPath='/home/wangrui/data/20news-18828/tfidf'
